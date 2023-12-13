4263fb9d7033e17fb071fd42f63649bce189a9a3
Unurbold99 <m.unurbold@letumongolia.com>
2023-12-13 18:16:33 +0800

    dd

--- Stock_app.py
+++ Stock_app.py
@@ -5,6 +5,7 @@ import pandas as pd
 import datetime
 import altair as alt
 import matplotlib.pyplot as plt
+from matplotlib.ticker import FuncFormatter
 
 # Function to scrape data for a specific company
 def scrape_data(url, company_name):
@@ -122,7 +123,7 @@ if st.button("Show Graph"):
     # Plot the data using matplotlib
     if not combined_data.empty:
         plt.figure(figsize=(12, 6))
-        
+    
         for selected_company in selected_companies:
             company_data = combined_data[combined_data['Company'] == selected_company]
             plt.plot(company_data['Date'], company_data['Highest Price'], label=selected_company)
@@ -131,4 +132,12 @@ if st.button("Show Graph"):
         plt.ylabel('Highest Price')
         plt.title('Stock Price Comparison')
         plt.legend()
+
+        # Set y-axis ticks to display only thousands
+        def format_thousands(x, pos):
+            return f'{int(x / 1000)}K'
+
+        formatter = FuncFormatter(format_thousands)
+        plt.gca().yaxis.set_major_formatter(formatter)
+
         st.pyplot(plt)
