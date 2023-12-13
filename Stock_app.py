adbc8ab32b6fe03d9f7b3550f0d7849b19fd5507
Unurbold99 <m.unurbold@letumongolia.com>
2023-12-13 17:19:06 +0800

    dd

--- Stock_app.py
+++ Stock_app.py
@@ -3,69 +3,32 @@ import pandas as pd
 import requests
 from bs4 import BeautifulSoup
 
-# Function to scrape data and return the combined DataFrame
-@st.cache(allow_output_mutation=True)
-def scrape_top_20_data():
-    starting_url = 'https://www.mse.mn/en/mse_top_20/266'
-    response = requests.get(starting_url)
-    html_content = response.content
+# Function to scrape data for a specific company and return the DataFrame
+@st.cache
+def scrape_company_data(url):
+    response = requests.get(url)
 
     if response.status_code == 200:
-        soup = BeautifulSoup(html_content, 'html.parser')
-        table = soup.find('table', class_='table dividend trade_table table-bordered table-striped table-hover table-condensed')
+        html = response.text
+        soup = BeautifulSoup(html, 'html.parser')
+        table = soup.find('table', class_='table table-bordered trade_history_result table-striped table-hover table-condensed')
 
         if table:
             tbody = table.find('tbody')
 
             if tbody:
-                urls = []
-                names = []
+                data = []
                 for row in tbody.find_all('tr'):
-                    link = row.find('a')
-                    if link:
-                        href = link.get('href')
-                        full_url = f"https://www.mse.mn{href}"
-                        urls.append(full_url)
+                    columns = row.find_all('td')
 
-                        name_text = link.get_text(strip=True)
-                        names.append(name_text)
+                    if len(columns) >= 8:
+                        second_column = columns[1].text.strip()
+                        sixth_column = columns[5].text.strip()
+                        eighth_column = columns[7].text.strip()
+                        data.append({'Highest Price': second_column, 'Volume': sixth_column, 'Date': eighth_column})
 
-                url_df = pd.DataFrame({'URLs': urls, 'Names': names})
-                dfs = []
-
-                for i, row in url_df.iterrows():
-                    url = row['URLs']
-                    company_name = row['Names']
-                    response = requests.get(url)
-
-                    if response.status_code == 200:
-                        html = response.text
-                        soup = BeautifulSoup(html, 'html.parser')
-                        table = soup.find('table', class_='table table-bordered trade_history_result table-striped table-hover table-condensed')
-
-                        if table:
-                            tbody = table.find('tbody')
-
-                            if tbody:
-                                data = []
-                                for row in tbody.find_all('tr'):
-                                    columns = row.find_all('td')
-
-                                    if len(columns) >= 8:
-                                        second_column = columns[1].text.strip()
-                                        sixth_column = columns[5].text.strip()
-                                        eighth_column = columns[7].text.strip()
-                                        data.append({'Highest Price': second_column, 'Volume': sixth_column, 'Date': eighth_column, 'Company': company_name})
-
-                                df = pd.DataFrame(data)
-                                dfs.append(df)
-                        else:
-                            st.warning(f"No table with the specified class found for {company_name}")
-                    else:
-                        st.warning(f"Failed to retrieve the webpage. Status Code: {response.status_code}")
-
-                combined_df = pd.concat(dfs, ignore_index=True)
-                return combined_df
+                df = pd.DataFrame(data)
+                return df
             else:
                 st.warning("No tbody found in the table.")
         else:
@@ -76,21 +39,39 @@ def scrape_top_20_data():
 # Streamlit App
 st.title("Stock Price Data App")
 
-# Button to trigger scraping
+# Button to trigger scraping for all companies
 if st.button("Scrape Top 20 data"):
-    scraped_data = scrape_top_20_data()
+    starting_url = 'https://www.mse.mn/en/mse_top_20/266'
+    response = requests.get(starting_url)
+
+    if response.status_code == 200:
+        soup = BeautifulSoup(response.content, 'html.parser')
+        table = soup.find('table', class_='table dividend trade_table table-bordered table-striped table-hover table-condensed')
+
+        if table:
+            tbody = table.find('tbody')
+
+            if tbody:
+                company_data = {}
+                for row in tbody.find_all('tr'):
+                    link = row.find('a')
+                    if link:
+                        href = link.get('href')
+                        full_url = f"https://www.mse.mn{href}"
+                        company_name = link.get_text(strip=True)
+
+                        # Scrape data for the current company
+                        df = scrape_company_data(full_url)
+
+                        # Store the DataFrame for the current company
+                        company_data[company_name] = df
+
     st.success("Data successfully scraped!")
 
-# Check if scraped_data is defined (i.e., if the button has been pressed)
-if 'scraped_data' in locals():
+# Check if company_data is defined (i.e., if the button has been pressed)
+if 'company_data' in locals():
     # Select box for company names
-    selected_company = st.selectbox("Select a Company", scraped_data['Company'].unique(), key="company_select")
+    selected_company = st.selectbox("Select a Company", list(company_data.keys()), key="company_select")
 
-    # Input field for the number of months to display
-    period_months = st.number_input("Period (months)", min_value=1, max_value=120, value=12)
-
-    # Filter data based on selected company and period
-    selected_data = scraped_data[scraped_data['Company'] == selected_company].tail(period_months)
-
-    # Display wider plot
-    st.line_chart(selected_data.set_index('Date')[['Highest Price']], use_container_width=True, width=800)
+    # Display the selected company's data
+    st.write(f"Data for {selected_company}:\n{company_data[selected_company]}")
